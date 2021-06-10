a62415816b58ad383c4e287eff00d3d63e3bf31a
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-06-09 21:59:01 -0400

    passed the first test 'if request file name is empty' with mocking flask request

---
+++ test.py
@@ -0,0 +1,25 @@
+import unittest
+import mock
+from app import upload
+
+
+class TestAppUploadFunction(unittest.TestCase):
+
+    def test_empty_filename(self):
+
+        mock_file_object = mock.MagicMock()
+        mock_file_object.filename = ''
+
+        mock_flask_request = mock.MagicMock()
+        mock_flask_request.files = {'file': mock_file_object}
+
+        expected_response = {'ERROR': "No file name was provided!"}, 400
+        with mock.patch('app.request', mock_flask_request):
+            # call upload function from app.py
+            actual_response = upload()
+
+        self.assertEqual(expected_response, actual_response)
+
+
+if __name__ == '__main__':
+    unittest.main()
